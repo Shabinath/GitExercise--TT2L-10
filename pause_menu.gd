da19ea1417fd3ee0c666de307ed784b2999b5fbd
pavien raj <pavienrajmuruges@gmail.com>
2024-06-15 19:52:54 +0800

    update and few changes

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,16 +1,8 @@
 extends Control
 
-func _ready():
-	# Connect the signals to the respective methods
-	$ColorRect/CenterContainer/VBoxContainer/restart.connect("pressed", Callable(self, "_on_restart_pressed"))
-	$ColorRect/CenterContainer/VBoxContainer/QuitButton2.connect("pressed", Callable(self, "_on_quit_button_2_pressed"))
 
 func resume():
 	get_tree().paused = false
-
-func pause():
-	get_tree().paused = true
-
 
 func _on_quit_button_2_pressed():
 	get_tree().quit()
@@ -18,6 +10,4 @@ func _on_quit_button_2_pressed():
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
 
-func _on_quit_button_pressed():
-	get_tree().quit()
 
